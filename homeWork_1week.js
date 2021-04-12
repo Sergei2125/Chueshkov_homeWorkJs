fa330d8bde3eb86973d610f139a46ad172e13395
Sergei <you@example.com>
2021-04-12 22:35:31 +0300

    howeWork_1week_2.0

--- homeWork_1week.js
+++ homeWork_1week.js
@@ -3,16 +3,16 @@
 //Exersize №1
 
 const sayHello = name => {
-    if(name==='Mark') {
+    if(name.toLowerCase() === 'mark') {
         console.log(`Hi ${name}!`)
     } else console.log(`Hello ${name}!`)
 };
 
 const sayHello2 = function(name){
-    if(name==='Mark') {
+    if(name.toLowerCase() === 'mark') {
         console.log(`Hi ${name}!`)
     } else console.log(`Hello ${name}!`)
-}
+};
 
 sayHello('Oleg');
 sayHello('Viktor');
@@ -20,22 +20,22 @@ sayHello('Mark');
 
 sayHello2('Oleg')
 sayHello2('Viktor')
-sayHello2('Mark')
+sayHello2('mark')
 
 console.log('-----------------------')
 
 //Exersize №2 У прямоугольного треугольника две стороны n (со значением 3) и m (со значением 4).
 //Найдите гипотенузу k по теореме Пифагора (нужно использовать функцию Math.pow(число, степень) или оператор возведения в степень ** ).
 
-const calcGipotenuza = function(n,m){
-    const gipotenuza = Math.sqrt(Math.pow(n,2)+Math.pow(m,2));
+const calcGipotenuza = function(n,m) {
+    const gipotenuza = Math.sqrt(Math.pow(n,2) + Math.pow(m,2)); 
     console.log(`Гипотенуза треугольника равна: ${gipotenuza}`);
 };
 
 calcGipotenuza(3,4);
 
-const calcGipotenuza2=(n,m)=>{
-    const gipotenuza = Math.sqrt(Math.pow(n,2)+Math.pow(m,2));
+const calcGipotenuza2 = (n,m) => {
+    const gipotenuza = Math.sqrt(Math.pow(n,2) + Math.pow(m,2));
     console.log(`Гипотенуза треугольника равна: ${gipotenuza}`);
 };
 
@@ -46,12 +46,11 @@ console.log('-----------------------')
 //Exersize 3
 
 const min = function(a,b){
-    if(a<b){
-        console.log(`Меньшее из чисел: ${a}`)
-    } else if(b<a) {
-        console.log(`Меньшее из чисел: ${b}`)
-    } else {
+    if(a === b){
         console.log(`Числа равны`)
+    } else {
+        result = Math.min(a,b)
+        console.log(`Меньшее из чисел: ${result}`)
     }
 };
 
@@ -59,13 +58,12 @@ min(2,5);
 min(3,-1);
 min(1,1);
 
-const min2=(a,b)=>{
-    if(a<b){
-        console.log(`Меньшее из чисел: ${a}`)
-    } else if(b<a) {
-        console.log(`Меньшее из чисел: ${b}`)
-    } else {
+const min2 = (a,b) => {
+    if(a === b) {
         console.log(`Числа равны`)
+    } else {
+        result = Math.min(a,b)
+        console.log(`Меньшее из чисел: ${result}`)
     }
 };
 
@@ -78,38 +76,28 @@ console.log('-----------------------')
 //Exersize 4 Определить является ли функция четным числом
 
 const isEven = function(n){
-    if(n%2===0) { 
-        return console.log(true)
-    }
-    console.log(false)
+    return n % 2 === 0;
 };
 
-isEven(3);
-isEven(4);
-isEven(5);
-isEven(6);
+console.log(isEven(3));
+console.log(isEven(4));
+console.log(isEven(5));
+console.log(isEven(6));
 
 const isEven2 = n => {
-    if(n%2===0) { 
-        return console.log(true)
-    }
-    console.log(false)  
+        return n % 2 === 0;  
 };
-isEven2(3);
-isEven2(4);
-isEven2(5);
-isEven2(6);
+console.log(isEven2(3));
+console.log(isEven2(4));
+console.log(isEven2(5));
+console.log(isEven2(6));
 
 console.log('-----------------------')
 
 //Exersize 5 
 
 const deleteChars = function(str){
-    const arrayStr = str.split('');
-    //console.log(arrayStr);
-    arrayStr.pop();
-    arrayStr.shift();
-    const result = arrayStr.join('');
+    const result = str.slice(1,str.length-1);
     console.log(result)
 };
 
@@ -117,7 +105,7 @@ deleteChars('Hello');
 deleteChars('A');
 
 const deleteChars2 = str => {
-    const result = str.split('').slice(1,str.length-1).join('');
+    const result = str.slice(1,str.length-1);
     console.log('результат: '+ result)
 };
 
@@ -162,7 +150,7 @@ console.log('-----------------------')
 
 //Exersize 7
 
-const getInversetype = function(str){
+const getInverseType = function(str){
     const newArray = [];
     for(i=0;i<str.length; i++){
         if(typeof(str[i])==='number') {
@@ -175,9 +163,9 @@ const getInversetype = function(str){
     console.log(newArray)
 };
 
-getInversetype([1,2,3,4,'6','7','8','9']);
+getInverseType([1,2,3,4,'6','7','8','9']);
 
-const getInversetype2 = str => {
+const getInverseType2 = str => {
     const newArray = [];
     for(i=0;i<str.length; i++){
         if(typeof(str[i])==='number') {
@@ -192,20 +180,20 @@ const getInversetype2 = str => {
     console.log(newArray)
 }
 
-getInversetype2([1,2,3,4,'6','7','8','9']);
+getInverseType2([1,2,3,4,'6','7','8','9']);
 
 console.log('-----------------------')
 
 //Exersize 8
 
-const getSumMassiv = function(str1,str2){
-    const newArray = str1.concat(str2);
+const getSumMassiv = function(array1,array2){
+    const newArray = [...array1,...array2]
     console.log(newArray)
 };
 getSumMassiv([1,2,3],[4,5,6]);
 
-const getSumMassiv2 = (str1,str2) => {
-    const newArray = str1.concat(str2);
+const getSumMassiv2 = (array1,array2) => {
+    const newArray = [...array1,...array2];
     console.log(newArray)
 };
 
@@ -216,18 +204,14 @@ console.log('-----------------------')
 //Exersize 9
 
 const stringCutter = function(str, num){
-    const newArray = str.split('');
-    newArray.splice(num, str.length, '...')
-    const result = newArray.join('')
+    const result = str.slice(0,num) + '...';
     console.log(result)
 }
 
 stringCutter('Привет Мир!', 6);
 
 const stringCutter2 = (str, num) => {
-    const newArray = str.split('');
-    newArray.splice(num, str.length, '...')
-    const result = newArray.join('')
+    const result = str.slice(0,num) + '...';
     console.log(`первые ${num} выделенных букв из строки + многоточие: ${result}`)
 }
 
